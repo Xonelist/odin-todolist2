e95d8ad5cf6cb325ea2c14315e40316cd6e902ca
Xonelist <83855246+Xonelist@users.noreply.github.com>
2023-12-17 23:35:14 +0700

    add some function for manipulating DOM

--- src/DOMElement.js
+++ src/DOMElement.js
@@ -1,8 +1,26 @@
-function createElement(elem, classname, content,id ='') {
-    const div = document.createElement('div');
-    div.className = classname;
-    div.id = id;
-
+function createElement(elem, classname, content='', id ='') {
     const element = document.createElement(elem);
     element.textContent = content;
+    element.className = classname;
+    element.id = id;
+    return element;
 }
+
+function createDiv(className, id='') {
+    const div = createElement('div', className, '', id);
+
+    return div;
+}
+
+function appendAll(to, ...arr) {
+    arr.forEach(dom => {
+        to.appendChild(dom);
+    });
+}
+
+function createButton(classname, buttontype, content, id='') {
+    const btn = createElement('button', classname, content, id);
+    btn.type = buttontype;
+}
+
+export {createDiv, createElement, createButton, appendAll} 
